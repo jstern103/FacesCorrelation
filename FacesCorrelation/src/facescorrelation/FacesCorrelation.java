0ef42b925c3448ec0d0f01fc21046227eb765542
hsexton1214 <Hannah@10.120.189.198>
2015-04-03 18:05:27 -0400

    parsing file

--- FacesCorrelation/src/facescorrelation/FacesCorrelation.java
+++ FacesCorrelation/src/facescorrelation/FacesCorrelation.java
@@ -5,6 +5,10 @@
  */
 package facescorrelation;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author Jacob Stern <jstern@unca.edu>
@@ -12,11 +16,82 @@ package facescorrelation;
  */
 public class FacesCorrelation {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void readFile(String newFile) throws FileNotFoundException {
+        File fileName = new File(newFile);
+        Scanner file = new Scanner(fileName);
+        file.nextLine();
+        file.useDelimiter("\\s*\\|\\s*");
+        while (file.hasNextLine()) {
+            int raterId = file.nextInt();
+            System.out.println(raterId);
+            int imageId = file.nextInt();
+
+            int group;
+            if (imageId >= 1 && imageId <= 15) {
+                group = 1;
+            } else if (imageId >= 16 && imageId <= 30) {
+                group = 2;
+            } else if (imageId >= 31 && imageId <= 45) {
+                group = 3;
+            } else {
+                group = 0;
+            }
+            System.out.println("   " + group);
+            file.next();
+            double[] attractiveness = new double[15];
+            attractiveness[0] = file.nextDouble();
+            System.out.println("   " + attractiveness[0]);
+            String blah = file.nextLine();
+            for (int i = 1; i < 15; i++) {
+                
+                String line = file.nextLine();
+                Scanner newLine = new Scanner(line);
+                newLine.useDelimiter("\\s*\\|\\s*");
+                int newId = newLine.nextInt();
+                if (newId == raterId) {
+                    newLine.next();
+                    newLine.next();
+                    attractiveness[i] = newLine.nextDouble();
+                    System.out.println("   " + attractiveness[i]);
+                }
+            }
+
+        }
+        /*while (file.hasNextLine()) {
+         int raterId = file.nextInt();
+         System.out.print(raterId);
+         file.next();
+
+         int imageId = file.nextInt();
+         file.next();
+         int group;
+         if (imageId >= 1 && imageId <= 15) {
+         group = 1;
+         } else if (imageId >= 16 && imageId <= 30) {
+         group = 2;
+         } else if (imageId >= 31 && imageId <= 45) {
+         group = 3;
+         } else {
+         group = 0;
+         }
+         double[] attractiveness = new double[15];
+         attractiveness[0] = file.nextDouble();
+
+         for (int i = 1; i < 15; i++) {
+         String line = file.nextLine();
+         Scanner newLine = new Scanner(line);
+         if (newLine.nextInt() == raterId) {
+         attractiveness[i] = newLine.nextDouble();
+         } 
+         }
+               
+
+         }*/
     }
-    
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readFile("FacesNormal4.txt");
+
+    }
+
 }
